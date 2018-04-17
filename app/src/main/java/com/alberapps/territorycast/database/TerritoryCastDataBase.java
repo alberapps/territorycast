af0f0e99b165dbc88d1c84bdfe2bee02890c1a84
Alberto Montiel <alberapps@gmail.com>
2018-04-17 20:13:01 +0200

    Preparar subida a Github

--- app/src/main/java/com/alberapps/territorycast/database/TerritoryCastDataBase.java
+++ app/src/main/java/com/alberapps/territorycast/database/TerritoryCastDataBase.java
@@ -1,3 +1,21 @@
+/**
+ *  TerritoryCast
+ *  Copyright (C) 2018 Alberto Montiel
+ *
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.alberapps.territorycast.database;
 
 import android.arch.persistence.room.Database;
@@ -7,9 +25,6 @@ import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 import android.support.annotation.VisibleForTesting;
 
-/**
- * Created by albert on 05/02/18.
- */
 
 /**
  * The Room database.
